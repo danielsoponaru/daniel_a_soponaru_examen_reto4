cc5503105581f01f704fe8c321b63b37b9ad0252
Daniel A. Soponaru <danielalejandro.sopo@alumni.mondragon.edu>
2025-06-04 08:35:28 +0200

    exploracion completada

--- ExamenReto4.R
+++ ExamenReto4.R
@@ -9,3 +9,42 @@ library(readxl)
 library(smoothmest)
 library(VIM)
 library(plumber)
+
+datos <- read.csv("netflix.csv")
+
+# Exploracion
+max(datos$Minutes, na.rm = T)
+max(datos$Seasons, na.rm = T)
+
+quantile(datos$Minutes, 0.75, na.rm = T)
+quantile(datos$Seasons, 0.75, na.rm = T)
+
+median(datos$Minutes, na.rm = T)
+median(datos$Seasons, na.rm = T)
+
+mean(datos$Minutes, na.rm = T)
+mean(datos$Seasons, na.rm = T)
+
+quantile(datos$Minutes, 0.25, na.rm = T)
+quantile(datos$Seasons, 0.25, na.rm = T)
+
+min(datos$Minutes, na.rm = T)
+min(datos$Seasons, na.rm = T)
+
+cuenta_nas_min <- datos %>% 
+  group_by(Minutes) %>% 
+  summarize(conteo = n())
+
+cuenta_nas_sea <- datos %>% 
+  group_by(Seasons) %>% 
+  summarize(conteo = n())
+
+sd(datos$Minutes, na.rm = T)
+sd(datos$Seasons, na.rm = T)
+
+# Elementos unicos
+
+tipos <- unique(datos$type)
+paises <- unique(datos$country)
+director <- unique(datos$director)
+
